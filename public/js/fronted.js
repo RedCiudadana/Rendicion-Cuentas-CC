4d0a27de6c4b274bbbdaf038e7243129335078af
Diego Auyón <diegoeauyonh@gmail.com>
2018-08-21 15:52:00 -0600

    Work in component

--- public/js/fronted.js
+++ public/js/fronted.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  // ** NAVBARS **
+
   // Check for click events on the navbar burger icon
   $(".navbar-burger").click(function() {
 
@@ -9,12 +11,27 @@ $(document).ready(function() {
 
   });
 
+  // ** MODALS **
+
   $(".modal-button").click(function() {
     $("#" + $(this).data("target")).toggleClass("is-active");
   });
 
   $('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button').click(function() {
-    $("#modal-ter").removeClass("is-active");
+    $(".modal").removeClass("is-active");
+  });
+
+  // ** TABS **
+  $('#tabs li').on('click', function() {
+    var tab = $(this).data('tab');
+
+    $('#tabs li').removeClass('is-active');
+    $(this).addClass('is-active');
+
+    $('#tab-content p').removeClass('is-active');
+    $('p[data-content="' + tab + '"]').addClass('is-active');
   });
 
 });
+
+
