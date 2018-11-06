b60061be8aeb47426a3e6102194d374739365b0f
D3AH <diegoeauyonh@gmail.com>
2018-11-06 11:47:52 -0600

    Eliminando chart-line

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,37 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  chartOptions: {
+    chart: {
+      type: 'line'
+    },
+    title: {
+      text: 'Número de expedientes ingresados'
+    },
+    xAxis: {
+      categories: ['Primer Informe', 'Segundo Informe', 'Tercer Informe', 'Cuarto Informe', 'Quinto Informe']
+    },
+    yAxis: {
+      title: {
+        text: 'Cantidad (Total)'
+      }
+    },
+    tooltip: {
+      valueSuffix: ' expedientes'
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      borderWidth: 0
+    }
+  },
+
+  chartData: [
+    {
+      name: 'Expedientes ingresados',
+      data: [400, 450, 500, 600, 500]
+    }
+  ]
 
 });
